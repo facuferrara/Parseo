b9fd712571dece5069d5ebebfa34289a82bbb418
pablo-origlia <pablo.origlia@estudiantes.unahur.edu.ar>
2024-10-31 04:22:42 -0300

    Se agrega delay entre acciones y excepcion cuando no existe archivo fuente.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import argparse
+import time
 import tkinter as tk
 from parser import build_parser  # Importa la función que construye el parser
 from lexer import lexer  # Importa el lexer desde lexer.py
 from math import cos, sin, radians
-
 # Inicializar la ventana y el canvas de Tkinter
 root = tk.Tk()
 root.title("Intérprete Tortuga")
@@ -22,7 +22,9 @@ def avanzar(distancia):
     new_x = x + distancia * cos(radians(angle))
     new_y = y - distancia * sin(radians(angle))
     if lapiz_abajo:
+        time.sleep(0.7)
         canvas.create_line(x, y, new_x, new_y)
+        canvas.update()        
     x, y = new_x, new_y
 
 def girar_izquierda(grados):
@@ -36,30 +38,25 @@ def girar_derecha(grados):
 # Construir el parser con las funciones de movimiento
 parser = build_parser(avanzar, girar_izquierda, girar_derecha)
 
-#tendria que abrir el archivo ejemplo.lg y cargar el codigo en la variable codigo
-
-cli = argparse.ArgumentParser()
-cli.add_argument("-v", "--verbose", help="Mostrar información de depuración", action="store_true")
+cli = argparse.ArgumentParser(description='Un simple compilador de Logo.')
 cli.add_argument("-f", "--file", help="Nombre de archivo a procesar")
+
 args = cli.parse_args()
- 
-# Aquí procesamos lo que se tiene que hacer con cada argumento
-if args.verbose:
-    print ("depuración activada!!!")
- 
+
+codigo = ""
+
 if args.file:
-    # print ("El nombre de archivo a procesar es: ", args.file)
-    
-    with open(args.file, encoding="utf8") as file:  
-        codigo = file.read()
-    # print(codigo)
-    
-#Utiliza el lexer y el parser para procesar el código
-resultado = parser.parse(codigo, lexer=lexer)
+    try:
+        with open(args.file, encoding="utf8") as file: 
+            codigo = file.read()
+    except IOError as e:
+        print(f"Archivo no encontrado: '{args.file}'")
 
-# Ejecuta las acciones generadas por el parser
-for accion in resultado:
-    accion()
-
-# Ejecutar el bucle principal de la ventana Tkinter
-root.mainloop()
+if codigo:
+    #Utiliza el lexer y el parser para procesar el código
+    resultado = parser.parse(codigo, lexer=lexer)
+    # Ejecuta las acciones generadas por el parser
+    for accion in resultado:
+        accion()
+    # Ejecutar el bucle principal de la ventana Tkinter
+    root.mainloop()
