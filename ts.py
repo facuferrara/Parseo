c428af1e3c4148dd91bccfbd6cb1a0ec5d868bc6
pablo-origlia <pablo.origlia@estudiantes.unahur.edu.ar>
2024-11-07 20:42:24 -0300

    Se agregan excepciones a la tabla de simbolos.

--- ts.py
+++ ts.py
@@ -1,33 +1,47 @@
+"""
+    Módulo para el manejo de la tablas de símbolos.
+"""
 from enum import Enum
 
-class TIPO_DATO(Enum) :
+
+class TIPO_DATO(Enum):
+    """
+    Esta clase representa los tipo de datos admitidos.
+    """
     NUMERO = 1
 
-class Simbolo() :
-    'Esta clase representa un simbolo dentro de nuestra tabla de simbolos'
 
-    def __init__(self, id, tipo, valor) :
-        self.id = id
+class Simbolo():
+    """
+    Esta clase representa un símbolo dentro de nuestra tabla de símbolos.
+    """
+
+    def __init__(self, id_key, tipo, valor):
+        self.id_key = id_key
         self.tipo = tipo
         self.valor = valor
 
-class TablaDeSimbolos() :
-    'Esta clase representa la tabla de simbolos'
 
-    def __init__(self, simbolos = {}) :
+class TablaDeSimbolos():
+    """
+    Esta clase representa la tabla de símbolos.
+    """
+
+    def __init__(self, simbolos=dict()):
         self.simbolos = simbolos
 
-    def agregar(self, simbolo) :
-        self.simbolos[simbolo.id] = simbolo
-    
-    def obtener(self, id) :
-        if not id in self.simbolos :
-            print('Error: variable ', id, ' no definida.')
+    def agregar(self, simbolo: Simbolo):
+        self.simbolos[simbolo.id_key] = simbolo
 
-        return self.simbolos[id]
+    def obtener(self, id_key: str) -> Simbolo:
+        try:
+            return self.simbolos[id_key]
+        except Exception as e:
+            raise NameError(f"Error: variable {id_key} no definida.") from e
 
-    def actualizar(self, simbolo) :
-        if not simbolo.id in self.simbolos :
-            print('Error: variable ', simbolo.id, ' no definida.')
-        else :
-            self.simbolos[simbolo.id] = simbolo
+    def actualizar(self, simbolo: Simbolo):
+        try:
+            self.simbolos[simbolo.id_key] = simbolo
+        except Exception as e:
+            raise NameError(f"Error: variable {
+                            simbolo.id_key} no definida.") from e
